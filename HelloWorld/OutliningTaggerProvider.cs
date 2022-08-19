708c8cfd690c7c0d35801e8c30fe8d9db5cb4db0
yassi <yassi@LAPTOP-3CFS8ESV>
2022-08-18 21:39:50 -0400

    Add project files.

---
+++ HelloWorld/OutliningTaggerProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+using System.Diagnostics.Contracts;
+using Microsoft.VisualStudio.Text;
+
+[Export(typeof(ITaggerProvider))]
+[TagType(typeof(IOutliningRegionTag))]
+[ContentType("text")]
+internal sealed class OutliningTaggerProvider : ITaggerProvider
+{
+    public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+    {
+        //create a single tagger for each buffer.
+        Func<ITagger<T>> sc = delegate () { return new OutliningTagger(buffer) as ITagger<T>; };
+        return buffer.Properties.GetOrCreateSingletonProperty<ITagger<T>>(sc);
+    }
+
+}
